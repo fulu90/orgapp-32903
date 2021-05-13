179bf725a2a22e9154a3cc267c5acff39bdb7192
fulu90 <giantsspirit2016@gmail.com>
2021-05-13 21:44:32 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'comments/index'
   root to: "rooms#index"
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :comments, only: [:index, :create]
+  end
 end
